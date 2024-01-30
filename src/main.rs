e12cea3a564c6b90e020bf07f022ba78d312016d
Aidan K <ak@umorpha.io>
2024-01-30 10:22:24 -0700

    create main file
    main file successfully implamented with user instruction upon error

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 use std::{env, process};
-
+use word_counter::Filename;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    
+    let filename = Filename::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem with argumernt input: {}", err);
+        eprintln!("run example: cargo run -- file.txt");
+        process::exit(1);
+    });
+
+    if let Err(e) = word_counter::run(filename) {
+        eprintln!("Problem with argumernt output: {}",e);
+        eprintln!("run example: cargo run -- file.txt");
+        process::exit(1);
+    } 
 }
